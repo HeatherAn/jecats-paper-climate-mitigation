bec000d9ccd42cfbeefa2a2936c3853a6382b58e
Jakob Smretschnig <J.Smretschnig@tudelft.nl>
2026-07-20 15:00:43 +0200

    update colors

--- src/cane/colors.py
+++ src/cane/colors.py
@@ -1,39 +1,11 @@
 import seaborn as sns
 
-# https://colorbrewer2.org/#type=sequential&scheme=YlGnBu&n=5
-# colorblind safe
-# print friendly
-YlGnBu = [
-    "#ffffcc",  # yellow
-    "#a1dab4",  # green
-    "#41b6c4",  # blue-green
-    "#2c7fb8",  # blue
-    "#253494"  # dark blue
-]
-
-# Tableau colors
-colormap_tab = {
-    "ref (BADA3 Thales)": "tab:purple",
-    "BADA3": "tab:blue",
-    "BADA4": "tab:orange",
-    "OpenAP": "tab:red",
-    "Poll-Schumann": "tab:green",
-}
-
 # Seaborn colorblind
 colors = sns.color_palette("colorblind")
 sns_idx = {
     cname: i
     for i, cname in
     enumerate(["darkblue", "orange", "green", "red", "violet", "brown", "pink", "gray", "yellow", "lightblue"])
-}
-
-colormap_sns = {
-    "ref (BADA3 Thales)": colors[6],  #
-    "BADA3": colors[0],  #
-    "BADA4": colors[1],  # orange
-    "OpenAP": colors[3],  # red
-    "Poll-Schumann": colors[2],  # green
 }
 
 # https://www.nature.com/articles/nmeth.1618
